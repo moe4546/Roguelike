9bffd601c2896b27ceed2cb05a7f71dc07513ea6
moe4546 <horn.moritz@web.de>
2019-02-20 17:19:25 +0100

    init

---
+++ Roguelike 2D/Assets/Scripts/BoardManager.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class BoardManager : MonoBehaviour
+{
+    [Serializable] 
+    public class Count 
+    {
+        public int maximum;
+        public int minimum;
+
+        public Count(int min, int max)
+        {
+            maximum = max;
+            minimum = min;
+        }
+    }
+
+
+    public int columns = 8;
+    public int rows = 8;
+    public Count wallCount = new Count(5, 9);
+    public Count foodCount = new Count(1, 5);
+
+    public GameObject exit;
+    public GameObject[] floorTiles;
+    public GameObject[] wallTiles;
+    public GameObject[] foodTiles;
+    public GameObject[] enemyTiles;
+    public GameObject[] outerWallTiles;
+
+    private Transform boardHolder;
+    private List<Vector3> gridPositions = new List<Vector3>();
+
+
+    void InitializeList()
+    {
+        gridPositions.Clear();
+
+        for (int x = 1; x < columns - 1; x++)
+        {
+            for (int y = 1; y < rows - 1; y++)
+            {
+                gridPositions.Add(new Vector3(x, y, 0f));
+            }
+        }
+    }
+
+    void BoardSetup()
+    {
+        boardHolder = new GameObject("Board").transform;
+
+        for (int x = -1; x < columns + 1; x++)
+        {
+            for (int y = -1; y < rows + 1; y++)
+            {
+                GameObject toInsantiate = floorTiles[Random.Range(0, floorTiles.Length)];
+                if(x == -1 || x == columns || y == -1 || y == rows)
+                {
+                    toInsantiate = wallTiles[Random.Range(0, wallTiles.Length)];
+                }
+            }
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
