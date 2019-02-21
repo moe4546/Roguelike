be269d17ca34d34e5c4ec947648273fc2d4862e6
moe4546 <horn.moritz@web.de>
2019-02-21 19:06:15 +0100

    Board Manager

--- Roguelike 2D/Assets/Scripts/BoardManager.cs
+++ Roguelike 2D/Assets/Scripts/BoardManager.cs
@@ -62,20 +62,42 @@ public class BoardManager : MonoBehaviour
                 {
                     toInsantiate = wallTiles[Random.Range(0, wallTiles.Length)];
                 }
+
+                GameObject instance = Instantiate(toInsantiate, new Vector3(x, y, 0f), Quaternion.identity) as GameObject;
+                instance.transform.SetParent(boardHolder);
             }
         }
     }
 
-
-    // Start is called before the first frame update
-    void Start()
+    Vector3 RandomPosition()
     {
-        
+        int randomIndex = Random.Range(0, gridPositions.Count);
+        Vector3 randomPosition = gridPositions[randomIndex];
+        gridPositions.RemoveAt(randomIndex);
+        return randomPosition;
     }
 
-    // Update is called once per frame
-    void Update()
+    void LayoutObjectAtRandom(GameObject[] tileArray, int minimum, int maximum)
     {
-        
+        int objectCount = Random.Range(minimum, maximum);
+
+        for(int i = 0; i < objectCount; i++)
+        {
+            Vector3 randomPosition = RandomPosition();
+            GameObject tileChoice = tileArray[Random.Range(0, tileArray.Length)];
+            Instantiate(tileChoice, randomPosition, Quaternion.identity);
+        }
+    }
+
+
+    public void SetupScene(int level)
+    {
+        BoardSetup();
+        InitializeList();
+        LayoutObjectAtRandom(wallTiles, wallCount.minimum, wallCount.maximum);
+        LayoutObjectAtRandom(foodTiles, foodCount.minimum, foodCount.maximum);
+        int enemyCount = (int)Mathf.Log(level, 2f);
+        LayoutObjectAtRandom(enemyTiles, enemyCount, enemyCount);
+        Instantiate(exit, new Vector3(columns - 1, rows - 1, 0f), Quaternion.identity);
     }
 }
--- Roguelike 2D/Assets/Scripts/GameManager.cs
+++ Roguelike 2D/Assets/Scripts/GameManager.cs
@@ -4,10 +4,29 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public BoardManager boardScript;
+    public static GameManager instance = null;
+
+    private int level = 3;
+
+
+    void Awake()
     {
-        
+        if (instance == null)
+        {
+            instance = this;
+        } else
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+        boardScript = GetComponent<BoardManager>();
+        InitGame();
+    }
+
+    void InitGame()
+    {
+        boardScript.SetupScene(level);
     }
 
     // Update is called once per frame
